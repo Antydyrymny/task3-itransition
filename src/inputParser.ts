3b8ff5f597ef3ab62b8a11ae91b40cd06b15a101
Konstantin Danovich <KDanovich42@gmail.com>
2023-10-06 17:19:02 +0300

    input parser minor refactor

--- src/inputParser.ts
+++ src/inputParser.ts
@@ -4,20 +4,18 @@ export default class InputParser {
             'Error: Arguments must be unique, and their number should be odd and greater than 2\n';
         const argsExample = '\nExample arguments: rock, paper, scissors';
         console.log(`${argsError}${msg}${argsExample}`);
+        process.exit();
     }
 
     public inputIsValid(args: string[]) {
         if (args.length < 3) {
             this.makeErrorMsg('Please, enter at least 3 arguments');
-            process.exit();
         } else if (args.length % 2 === 0) {
             this.makeErrorMsg('Please, enter the odd number of arguments');
-            process.exit();
         }
         const argsSet = new Set(args);
         if (argsSet.size !== args.length) {
             this.makeErrorMsg('Please, make sure all arguments are unique');
-            process.exit();
         }
     }
 }
